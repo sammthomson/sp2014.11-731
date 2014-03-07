44de3ca325b6f735af2d0f9209a8dd06e10f2b92
Sam Thomson <sammthomson@gmail.com>
2014-03-06 23:55:24 -0500

    strip accents

--- hw2/train_test.sh
+++ hw2/train_test.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
+set -e
+
 ./extract > myfeatures.json
-#./fit --l1 5.0 < myfeatures.json > weights.json
+#./fit --l1 1.0 < myfeatures.json > weights.json
 ./fit --l2 1.0 < myfeatures.json > weights.json
 ./score -w weights.json < myfeatures.json > output.txt
 ./evaluate < output.txt
